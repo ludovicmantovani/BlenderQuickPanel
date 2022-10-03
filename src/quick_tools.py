cda4ba510cddc337ba6fb68e332b820679968285
ludovicmantovani <ludovic.mantov@gmail.com>
2022-10-03 16:27:19 +0200

    [FIX]: Reformat python file

--- src/quick_tools.py
+++ src/quick_tools.py
@@ -3,18 +3,17 @@ import bpy
 from bpy_extras.io_utils import ExportHelper, ImportHelper
 from bpy.types import Panel, Operator
 
+
 class RENDER_main_panel(Panel):
     bl_idname = "RENDER_main_panel"
     bl_label = "Render settings"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category = "Quick tools"
 
     def draw(self, context):
         layout = self.layout
-        layout.operator(
-            "freestyle_render_set_param_op.operator",
-            icon='RENDER_STILL')
+        layout.operator("freestyle_render_set_param_op.operator", icon="RENDER_STILL")
 
 
 class FREESTYLE_RENDER_set_param_op(Operator):
@@ -26,21 +25,21 @@ class FREESTYLE_RENDER_set_param_op(Operator):
         view_layer = bpy.context.scene.view_layers["ViewLayer"]
         view_layer.use_freestyle = True
         freestyle_settings = view_layer.freestyle_settings
-        freestyle_settings.crease_angle = 1.74533 # 100° in radian
+        freestyle_settings.crease_angle = 1.74533  # 100° in radian
         # bpy.ops.scene.freestyle_lineset_remove()
         while len(freestyle_settings.linesets) != 2:
-                if len(freestyle_settings.linesets) > 2:
-                    bpy.ops.scene.freestyle_lineset_remove()
-                else:
-                    bpy.ops.scene.freestyle_lineset_add()
+            if len(freestyle_settings.linesets) > 2:
+                bpy.ops.scene.freestyle_lineset_remove()
+            else:
+                bpy.ops.scene.freestyle_lineset_add()
         default_lineset = freestyle_settings.linesets[0]
         default_lineset.name = "Default"
-        default_lineset. select_silhouette = True
+        default_lineset.select_silhouette = True
         default_lineset.select_crease = False
         default_lineset.select_border = False
         default_lineset.select_edge_mark = True
         default_lineset.select_external_contour = True
-        
+
         crease_angle_lineset = freestyle_settings.linesets[1]
         crease_angle_lineset.name = "Crease angle"
         crease_angle_lineset.select_silhouette = False
@@ -48,24 +47,32 @@ class FREESTYLE_RENDER_set_param_op(Operator):
         crease_angle_lineset.select_border = False
         crease_angle_lineset.select_edge_mark = False
         crease_angle_lineset.select_external_contour = False
-        bpy.ops.scene.freestyle_alpha_modifier_add(type='CREASE_ANGLE')
+        bpy.ops.scene.freestyle_alpha_modifier_add(type="CREASE_ANGLE")
         crease_angle_lineset.linestyle.name = "Crease angle"
-        crease_angle_lineset.linestyle.alpha_modifiers["Crease Angle"].angle_min = 1.39626 # 80° in radian
-        crease_angle_lineset.linestyle.alpha_modifiers["Crease Angle"].angle_max = 1.74533 # 100° in radian
+        crease_angle_lineset.linestyle.alpha_modifiers[
+            "Crease Angle"
+        ].angle_min = 1.39626  # 80° in radian
+        crease_angle_lineset.linestyle.alpha_modifiers[
+            "Crease Angle"
+        ].angle_max = 1.74533  # 100° in radian
 
-        return {'FINISHED'}
+        return {"FINISHED"}
+
 
 class DISPLAY_COLOR_main_panel(Panel):
     bl_idname = "DISPLAY_COLOR_main_panel"
     bl_label = "Viewport display color"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category = "Quick tools"
-    
+
     def draw(self, context):
         layout = self.layout
 
-        layout.operator("display_color_reset_everything.operator", icon = "DECORATE_OVERRIDE")
+        layout.operator(
+            "display_color_reset_everything.operator", icon="DECORATE_OVERRIDE"
+        )
+
 
 class DISPLAY_COLOR_reset_everything(Operator):
     bl_label = "Reset"
@@ -78,25 +85,26 @@ class DISPLAY_COLOR_reset_everything(Operator):
                 print("Mesh", mesh.name, "slot", slot, "material", material)
                 background_node = material.node_tree.nodes.get("Background", None)
                 if background_node:
-                    material.diffuse_color = background_node.inputs["Color"].default_value
-        return {'FINISHED'}
+                    material.diffuse_color = background_node.inputs[
+                        "Color"
+                    ].default_value
+        return {"FINISHED"}
 
 
 class ARMATURE_NAME_REMAP_PT_main_panel(Panel):
     bl_label = "Armature rename"
     bl_idname = "ARMATURE_NAME_REMAP_PT_main_panel"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category = "Quick tools"
 
     def draw(self, context):
         layout = self.layout
 
-        layout.operator("armature_name_min_maj.myop_operator", icon = "SMALL_CAPS")
+        layout.operator("armature_name_min_maj.myop_operator", icon="SMALL_CAPS")
         layout.separator()
-        layout.operator("armature_name_export.myop_operator", icon = "EXPORT")
-        layout.operator("armature_name_remap.myop_operator", icon = "IMPORT")
-
+        layout.operator("armature_name_export.myop_operator", icon="EXPORT")
+        layout.operator("armature_name_remap.myop_operator", icon="IMPORT")
 
 
 class ARMATURE_NAME_MIN_MAJ_OT_my_op(Operator):
@@ -105,68 +113,68 @@ class ARMATURE_NAME_MIN_MAJ_OT_my_op(Operator):
 
     def execute(self, context):
         active = bpy.context.active_object
-        if active.type != 'ARMATURE':
-            self.report({'ERROR_INVALID_CONTEXT'}, "Active object is not an armature.")
+        if active.type != "ARMATURE":
+            self.report({"ERROR_INVALID_CONTEXT"}, "Active object is not an armature.")
         else:
             nbr = 0
             for b in active.data.bones:
-                if '_l' in b.name or '_r' in b.name:
-                    new_name = b.name.replace('_l','_L').replace('_r','_R')
+                if "_l" in b.name or "_r" in b.name:
+                    new_name = b.name.replace("_l", "_L").replace("_r", "_R")
                     b.name = new_name
-                    nbr+=1
-            self.report({'INFO'}, str(nbr) + " bones renamed")
-        return {'FINISHED'}
+                    nbr += 1
+            self.report({"INFO"}, str(nbr) + " bones renamed")
+        return {"FINISHED"}
 
 
 class ARMATURE_NAME_EXPORT_OT_my_op(Operator, ExportHelper):
     bl_label = "Extract bones name"
     bl_idname = "armature_name_export.myop_operator"
-    bl_options = {'PRESET', 'UNDO'}
-    
-    filename_ext = '.txt'
-    
+    bl_options = {"PRESET", "UNDO"}
+
+    filename_ext = ".txt"
+
     def execute(self, context):
         active = bpy.context.active_object
-        if active.type != 'ARMATURE':
-            self.report({'ERROR_INVALID_CONTEXT'}, "Active object is not an armature.")
+        if active.type != "ARMATURE":
+            self.report({"ERROR_INVALID_CONTEXT"}, "Active object is not an armature.")
         else:
-            bones_name = [b.name + ':' for b in active.data.bones]
+            bones_name = [b.name + ":" for b in active.data.bones]
             bones_name.sort()
-            formated_bones_name = '\n'.join(bones_name)
-            with open(self.filepath, 'w') as fd:
+            formated_bones_name = "\n".join(bones_name)
+            with open(self.filepath, "w") as fd:
                 fd.write(formated_bones_name)
-            self.report({'INFO'}, "Please, complete " + self.filepath)
-        return {'FINISHED'}
+            self.report({"INFO"}, "Please, complete " + self.filepath)
+        return {"FINISHED"}
 
 
 class ARMATURE_NAME_REMAP_OT_my_op(Operator, ImportHelper):
     bl_label = "Remap bones name"
     bl_idname = "armature_name_remap.myop_operator"
-    bl_options = {'PRESET', 'UNDO'}
-    
-    filename_ext = '.txt'
-    
+    bl_options = {"PRESET", "UNDO"}
+
+    filename_ext = ".txt"
+
     def execute(self, context):
-        self.report({'INFO'}, "Load from : " + self.filepath)
+        self.report({"INFO"}, "Load from : " + self.filepath)
         lines = []
-        
+
         with open(self.filepath) as fd:
             lines = [line.strip() for line in fd]
         translate = self.getTranslation(lines)
         self.remap(translate)
-            
-        return {'FINISHED'}
+
+        return {"FINISHED"}
 
     def getTranslation(self, lines):
         tr = {}
         for line in lines:
-            if len(line.split(':')) == 2:
-                old, new = line.split(':')
+            if len(line.split(":")) == 2:
+                old, new = line.split(":")
                 old = old.strip()
                 new = new.strip()
-                if new != '':
+                if new != "":
                     tr[old] = new
-                    self.report({'INFO'}, old +" : " + tr[old])
+                    self.report({"INFO"}, old + " : " + tr[old])
         return tr
 
     def remap(self, translate_dict):
@@ -176,8 +184,8 @@ class ARMATURE_NAME_REMAP_OT_my_op(Operator, ImportHelper):
             bone = active.data.bones.get(old, None)
             if bone:
                 bone.name = new
-                nbr +=1
-        self.report({'INFO'}, "Change " + str(nbr) + " bones name")
+                nbr += 1
+        self.report({"INFO"}, "Change " + str(nbr) + " bones name")
 
 
 classes = [
@@ -190,17 +198,17 @@ classes = [
     RENDER_main_panel,
     FREESTYLE_RENDER_set_param_op,
 ]
- 
- 
+
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-        
- 
+
+
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
- 
- 
+
+
 if __name__ == "__main__":
     register()
